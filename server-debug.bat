08f35c962c9481996d9780eb3ce8b05ba199a4bf
Daniel Schulz <faulolio@gmail.com>
2024-12-20 15:01:20 +0100

    build: add console window

--- server-debug.bat
+++ server-debug.bat
@@ -1,4 +1,4 @@
 setlocal
 FOR /F "tokens=*" %%i in ('type .env') do SET %%i
-call docker compose --file docker-compose-debug.yml up --detach --build
+start "Debug Server" docker compose --file docker-compose-debug.yml up --build
 endlocal
