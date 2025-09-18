dff9a7a964797f07756ac00e8d0cadf204d5caa5
Daniel Schulz <faulolio@gmail.com>
2025-09-18 14:47:39 +0200

    feat: update farah

--- src/Assets/EventAPI.php
+++ src/Assets/EventAPI.php
@@ -2,37 +2,43 @@
 declare(strict_types = 1);
 namespace Slothsoft\Server\HistorischerSpieleabend\Assets;
 
-use Slothsoft\Core\DOMHelper;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Module;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
 use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\DOMWriterResultBuilder;
+use Slothsoft\Farah\Sites\Domain;
 
 class EventAPI implements ExecutableBuilderStrategyInterface {
-
+    
     public static string $indexPath = __DIR__ . '/../../assets/index.xml';
-
+    
     public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies {
         $url = $context->createUrl()->withPath('/index');
         $writer = Module::resolveToDOMWriter($url);
         $result = new ExecutableStrategies(new DOMWriterResultBuilder($writer, 'index.xml'));
-
-        $xpath = DOMHelper::loadXPath(DOMHelper::loadDocument('farah://slothsoft@farah/sites'));
-        $eventId = $xpath->evaluate('string(//*[@current]/@name)');
-
+        
+        $domain = Domain::createWithDefaultSitemap();
+        
+        $pageNode = $domain->getCurrentPageNode();
+        if (! $pageNode) {
+            return $result;
+        }
+        
+        $eventId = $pageNode->getAttribute('name');
+        
         if (! $eventId) {
             return $result;
         }
-
+        
         $document = $writer->toDocument();
         $event = $document->getElementById($eventId);
-
+        
         if (! $event) {
             return $result;
         }
-
+        
         $hasChanged = false;
         foreach ($args->get('event', []) as $key => $value) {
             if (is_string($value) and $event->getAttribute($key) !== $value) {
@@ -40,11 +46,11 @@ class EventAPI implements ExecutableBuilderStrategyInterface {
                 $hasChanged = true;
             }
         }
-
+        
         if ($hasChanged) {
             $document->save(self::$indexPath);
         }
-
+        
         return $result;
     }
 }
